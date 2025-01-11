a4a07a4950a7727cdbd22b557b25ab2c0610a454
root <root@mail.rayanai.io>
2025-01-11 16:19:33 +0000

    ready to deploy

--- utils.py
+++ utils.py
@@ -80,9 +80,6 @@ async def transcribe_audio(audio_file: BytesIO) -> str:
         transcription = await client.audio.transcriptions.create(
             model="whisper-1", file=audio_file
         )
-
-        with open("trans.txt", "w") as f:
-            f.write(transcription.text)
         return transcription.text
     except Exception as e:
         print(e)
